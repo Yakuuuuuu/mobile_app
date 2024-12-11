29a6e15404bd696def5f35cc17f0bdd8f1ed08c8
Yakuuuuuu <phuyalsamrat8@gmail.com>
2024-12-11 11:18:56 +0545

    ok

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
-
-import 'package:batch33/view/columnview.dart';
+import 'package:batch33/view/calculator_view.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ColumnView(),
+      home: CalculatorView(),
     );
   }
 }
